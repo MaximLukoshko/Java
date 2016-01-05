691bde27001d2a28920450f83d32979127c54caa
MaximLukoshko <www.maxim.by97@gmail.com>
2016-01-05 22:29:48 +0300

    Added Empty WebChatServlet

---
+++ Servlets/src/servlet/WebChatServlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class WebChatServlet
+ */
+@WebServlet("/WebChatServlet")
+public class WebChatServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	
+	
+	
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
