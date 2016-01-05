a3fda48e1a1a610cd1f47fd3d8aadcfe5a3ab5d0
MaximLukoshko <www.maxim.by97@gmail.com>
2016-01-06 00:13:39 +0300

    Added all empty servlets

---
+++ Servlets/src/servlet/NewMessageServlet.java
@@ -0,0 +1,31 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NewMessageServlet
+ */
+@WebServlet("/send_message.do")
+public class NewMessageServlet extends WebChatServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public NewMessageServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
