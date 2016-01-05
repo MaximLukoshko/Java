f699505d9a9d13c2b08e8daa27728aed3ea1c456
MaximLukoshko <www.maxim.by97@gmail.com>
2016-01-05 22:41:31 +0300

    Written ChatServlet and added WebContent

--- Servlets/src/servlet/WebChatServlet.java
+++ Servlets/src/servlet/WebChatServlet.java
@@ -1,9 +1,15 @@
 package servlet;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+
+import entity.ChatMessage;
+import entity.ChatUser;
 
 /**
  * Servlet implementation class WebChatServlet
@@ -15,11 +21,24 @@ public class WebChatServlet extends HttpServlet {
 	/**
 	 * @see Servlet#init(ServletConfig)
 	 */
-	
-	
-	
+
+	HashMap<String, ChatUser> activeUsers;
+	ArrayList<ChatMessage> messages;
+
 	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
+		super.init();
+		activeUsers = (HashMap<String, ChatUser>) getServletContext()
+				.getAttribute("activeUsers");
+		messages = (ArrayList<ChatMessage>) getServletContext().getAttribute(
+				"messages");
+		if (activeUsers == null) {
+			activeUsers = new HashMap<String, ChatUser>();
+			getServletContext().setAttribute("activeUsers", activeUsers);
+		}
+		if (messages == null) {
+			messages = new ArrayList<ChatMessage>();
+			getServletContext().setAttribute("messages", messages);
+		}
 	}
 
 }
