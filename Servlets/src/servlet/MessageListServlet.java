a26eff696d370846ba22f47872a4634d0e42c5d5
MaximLukoshko <www.maxim.by97@gmail.com>
2016-01-06 13:42:45 +0300

    Added users printing

--- Servlets/src/servlet/MessageListServlet.java
+++ Servlets/src/servlet/MessageListServlet.java
@@ -30,21 +30,11 @@ public class MessageListServlet extends WebChatServlet {
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// Установить кодировку HTTP-ответа UTF-8
 		response.setCharacterEncoding("utf8");
-		// Получить доступ к потоку вывода HTTP-ответа
 		PrintWriter pw = response.getWriter();
-		// Записть в поток HTML-разметку страницы
 		pw.println("<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'/><meta http-equiv='refresh' content='10'></head>");
 		pw.println("<body>");
-		// В обратном порядке записать в поток HTML-разметку для каждого
-		// сообщения
-		// Установить кодировку HTTP-ответа UTF-8
 		response.setCharacterEncoding("utf8");
-		// Получить доступ к потоку вывода HTTP-ответа
-		// Записть в поток HTML-разметку страницы
-		// В обратном порядке записать в поток HTML-разметку для каждого
 		for (int i = messages.size() - 1; i >= 0; i--) {
 			ChatMessage aMessage = messages.get(i);
 			pw.println("<div><strong>" + aMessage.getAuthor().getName()
--- Servlets/src/servlet/UsersListServlet.java
+++ Servlets/src/servlet/UsersListServlet.java
@@ -1,10 +1,15 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import entity.ChatUser;
 
 /**
  * Servlet implementation class UsersListServlet
@@ -13,19 +18,28 @@ import javax.servlet.http.HttpServletResponse;
 public class UsersListServlet extends WebChatServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public UsersListServlet() {
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * Default constructor.
+	 */
+	public UsersListServlet() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		PrintWriter pw = response.getWriter();
+		pw.println("<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'/><meta http-equiv='refresh' content='10'></head>");
+		pw.println("<body>");
+		response.setCharacterEncoding("utf8");
+		for (ChatUser user : activeUsers.values()) {
+			pw.println("<div>" + "<strong>" + user.getName() + "</strong>"
+					+ "</div>");
+		}
 	}
 
 }
