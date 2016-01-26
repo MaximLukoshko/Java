2e9f3f1ec9636274958d633213c2291786ede383
MaximLukoshko <www.maxim.by97@gmail.com>
2016-01-26 15:42:52 +0300

    Added fields to frame

--- NetProg/src/main/MainFrame.java
+++ NetProg/src/main/MainFrame.java
@@ -3,11 +3,14 @@ package main;
 import java.awt.Dimension;
 import java.awt.HeadlessException;
 import java.awt.Toolkit;
-
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+import javax.swing.GroupLayout.Alignment;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame {
@@ -22,6 +25,10 @@ public class MainFrame extends JFrame {
 
 	private static final int INCOMING_AREA_DEFAULT_ROWS = 10;
 	private static final int OUTGOING_AREA_DEFAULT_ROWS = 5;
+
+	private static final int SMALL_GAP = 5;
+	private static final int MEDIUM_GAP = 10;
+	private static final int LARGE_GAP = 15;
 
 	private final JTextField textFieldFrom;
 	private final JTextField textFieldTo;
@@ -45,6 +52,45 @@ public class MainFrame extends JFrame {
 
 		textFieldTo = new JTextField(TO_FIELD_DEFAULT_COLUMNS);
 		textFieldFrom = new JTextField(FROM_FIELD_DEFAULT_COLUMNS);
+
+		// Панель ввода сообщения
+		final JPanel messagePanel = new JPanel();
+		messagePanel.setBorder(BorderFactory.createTitledBorder("Message"));
+
+		final GroupLayout layout2 = new GroupLayout(messagePanel);
+		messagePanel.setLayout(layout2);
+
+		layout2.setHorizontalGroup(layout2
+				.createSequentialGroup()
+				.addContainerGap()
+				.addGroup(
+						layout2.createParallelGroup(Alignment.TRAILING)
+								.addGroup(
+										layout2.createSequentialGroup()
+												.addComponent(textFieldFrom)
+												.addComponent(textFieldTo)))
+				.addContainerGap());
+		layout2.setVerticalGroup(layout2
+				.createSequentialGroup()
+				.addContainerGap()
+				.addGroup(
+						layout2.createParallelGroup(Alignment.BASELINE)
+								.addComponent(textFieldFrom)
+								.addComponent(textFieldTo)).addContainerGap());
+
+		final GroupLayout layout1 = new GroupLayout(getContentPane());
+		setLayout(layout1);
+
+		layout1.setHorizontalGroup(layout1
+				.createSequentialGroup()
+				.addContainerGap()
+				.addGroup(
+						layout1.createParallelGroup()
+								.addComponent(textAreaIncoming)
+								.addComponent(messagePanel)).addContainerGap());
+		layout1.setVerticalGroup(layout1.createSequentialGroup()
+				.addContainerGap().addComponent(textAreaIncoming)
+				.addComponent(messagePanel).addContainerGap());
 	}
 
 	public static void main(String[] args) {
