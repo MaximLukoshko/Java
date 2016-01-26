c078ab8d6bcd2892ea91cc57188acd91e68fb987
MaximLukoshko <www.maxim.by97@gmail.com>
2016-01-26 14:49:55 +0300

    Added variables for fields

--- NetProg/src/main/MainFrame.java
+++ NetProg/src/main/MainFrame.java
@@ -5,6 +5,8 @@ import java.awt.HeadlessException;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 @SuppressWarnings("serial")
@@ -15,6 +17,18 @@ public class MainFrame extends JFrame {
 	private static final int FRAME_MINIMUM_WIDTH = 500;
 	private static final int FRAME_MINIMUM_HEIGHT = 500;
 
+	private static final int FROM_FIELD_DEFAULT_COLUMNS = 10;
+	private static final int TO_FIELD_DEFAULT_COLUMNS = 20;
+
+	private static final int INCOMING_AREA_DEFAULT_ROWS = 10;
+	private static final int OUTGOING_AREA_DEFAULT_ROWS = 5;
+
+	private final JTextField textFieldFrom;
+	private final JTextField textFieldTo;
+
+	private final JTextArea textAreaIncoming;
+	private final JTextArea textAreaOutgoing;
+
 	public MainFrame() throws HeadlessException {
 		super(FRAME_TITLE);
 		// TODO Auto-generated constructor stub
@@ -23,6 +37,14 @@ public class MainFrame extends JFrame {
 		final Toolkit kit = Toolkit.getDefaultToolkit();
 		setLocation((kit.getScreenSize().width - getWidth()) / 2,
 				(kit.getScreenSize().height - getHeight()) / 2);
+
+		textAreaIncoming = new JTextArea(INCOMING_AREA_DEFAULT_ROWS, 0);
+		textAreaIncoming.setEditable(false);
+
+		textAreaOutgoing = new JTextArea(OUTGOING_AREA_DEFAULT_ROWS, 0);
+
+		textFieldTo = new JTextField(TO_FIELD_DEFAULT_COLUMNS);
+		textFieldFrom = new JTextField(FROM_FIELD_DEFAULT_COLUMNS);
 	}
 
 	public static void main(String[] args) {
