7566abccb13123a4d2644713201aa87ccd15f372
MaximLukoshko <www.maxim.by97@gmail.com>
2016-01-29 23:26:47 +0300

    Added classes Peer and MessageListener

--- NetProg/src/main/InstantMessenger.java
+++ NetProg/src/main/InstantMessenger.java
@@ -2,8 +2,16 @@ package main;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 public class InstantMessenger {
+	private ArrayList<MessageListener> listeners;
+
+	public InstantMessenger() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	public void sendMessage(String senderName, String destinationAddress,
 			String message) throws UnknownHostException, IOException {
 
@@ -12,6 +20,27 @@ public class InstantMessenger {
 	@SuppressWarnings("unused")
 	private void startServer() {
 
+	}
+
+	public void addMessageListener(MessageListener listener) {
+		synchronized (listeners) {
+			listeners.add(listener);
+		}
+	}
+
+	public void removeMessageListener(MessageListener listener) {
+		synchronized (listeners) {
+			listeners.remove(listener);
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private void notifyListeners(String sender, String message) {
+		synchronized (listeners) {
+			for (MessageListener listener : listeners) {
+				listener.messageReceived(sender, message);
+			}
+		}
 	}
 
 }
---
+++ NetProg/src/main/MessageListener.java
@@ -0,0 +1,7 @@
+package main;
+
+public class MessageListener {
+	void messageReceived(String senderName, String message) {
+
+	}
+}
