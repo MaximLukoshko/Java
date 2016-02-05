021f2c918ffeea047401690aa40c2415a254c23e
MaximLukoshko <www.maxim.by97@gmail.com>
2016-02-05 12:31:21 +0300

    Added empty tags and tag Descriptor

---
+++ JSP/src/tag/Login.java
@@ -0,0 +1,16 @@
+package tag;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class Login extends SimpleTagSupport {
+
+	@Override
+	public void doTag() throws JspException, IOException {
+		// TODO Auto-generated method stub
+		super.doTag();
+	}
+
+}
